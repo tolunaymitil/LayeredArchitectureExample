8a9b98883bc24202077f6a5c11be2eeb9bef56ee
ACER <ACER@ACER-BILGISAYAR>
2022-02-18 20:50:54 +0200

    Add project files.

---
+++ Abc.Northwind.Dataaccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abc.Northwind.Dataaccess.Concrete.EntityFramework
+{
+   public class EfProductDal
+    {
+    }
+}
